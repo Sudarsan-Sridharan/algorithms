4472eedc1d306df3ed22413c4684cb6b98d26857
Saurav Verma <viczsaurav@gmail.com>
2015-04-14 22:50:39 +0800

    LSD-Number Sort

--- src/RadixSort.java
+++ src/RadixSort.java
@@ -26,9 +26,33 @@ public class RadixSort {
 	         for (int r = 0; r < R; r++)
 	            count[r+1] += count[r];
 
-	         for (int i = 0; i < N; i++)
+	         for (int i = 0; i < N; i++){
+	        	 System.out.println(a[i].charAt(d)+"-"+ count[a[i].charAt(d)]);
 	        	 aux[count[a[i].charAt(d)]++] = a[i];
-	            
+	         }   
+	         for (int i = 0; i < N; i++)
+	            a[i] = aux[i];
+	      }
+	   }
+	
+	public static void LSDNumberSort(String[] a, int W)
+	   {
+	      int R = 256;
+	      int N = a.length;
+	      String[] aux = new String[N];
+	      for (int d = W-1; d >= 0; d--)
+	      {
+	         int[] count = new int[R+1];
+	         for (int i = 0; i < N; i++)
+	            count[a[i].charAt(d) + 1]++;
+
+	         for (int r = 0; r < R; r++)
+	            count[r+1] += count[r];
+
+	         for (int i = 0; i < N; i++){
+	        	 System.out.println(a[i].charAt(d)+"-"+ count[a[i].charAt(d)]);
+	        	 aux[count[a[i].charAt(d)]++] = a[i];
+	         }   
 	         for (int i = 0; i < N; i++)
 	            a[i] = aux[i];
 	      }
